478f1714bb8cd05940e4b10082914fa1a249106c
Dmitriy <ulrht88@gmail.com>
2023-05-03 01:02:48 +0300

    patch 1.0

--- script.py
+++ script.py
@@ -14,15 +14,15 @@ vk = session.get_api()
 
 def get_user_friends(user_id):
     friends = session.method('friends.get', {'user_id:': user_id})
-    friend_list = []
     count = 0
 
+    friend_list = []
     for friend in friends['items']:
         session.method('users.get', {'user_ids': friend})
         time.sleep(0.5)
-        if ['user.is_closed'] is True:
-            continue
         friend_list.append(friend)
+
+    session.method('groups.get', {'groups_name': user_id})
 
     group_list = []
     for groups in friend_list:
