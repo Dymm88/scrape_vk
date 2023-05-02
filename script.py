c33548576a3eb5a8f08ea8bcd6873b1cbeb760c1
Dmitriy <ulrht88@gmail.com>
2023-05-03 01:31:15 +0300

    patch 2.0

--- script.py
+++ script.py
@@ -13,25 +13,28 @@ vk = session.get_api()
 
 
 def get_user_friends(user_id):
-    friends = session.method('friends.get', {'user_id:': user_id})
-    count = 0
+    friends = session.method('friends.get', {'user_id': user_id})
+    # count = 0
 
-    friend_list = []
+    # friend_list = []
     for friend in friends['items']:
-        session.method('users.get', {'user_ids': friend})
+        user = session.method('users.get', {'user_ids': friend})
+        groups = session.method('groups.get', {'user_id': friend})
         time.sleep(0.5)
-        friend_list.append(friend)
+        print(f"{user[0]['first_name']} {user[0]['last_name']}")
+        print(f"{groups['items']}")
+        # friend_list.append(friend)
 
-    session.method('groups.get', {'groups_name': user_id})
-
-    group_list = []
-    for groups in friend_list:
-        session.method('groups.get', {'groups_name': groups})
-        time.sleep(0.5)
-        group_list.append(groups)
-        count += 1
-        print(count)
-    print(group_list)
+    # session.method('groups.get', {})
+    #
+    # group_list = []
+    # for groups in friend_list:
+    #     session.method('groups.get', {'groups_name': groups})
+    #     time.sleep(0.5)
+    #     group_list.append(groups)
+    #     count += 1
+    #     print(count)
+    # print(group_list)
 
 
 get_user_friends(int(input('enter id user: ')))
