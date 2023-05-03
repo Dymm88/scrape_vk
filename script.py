5df0c84dc4e19d1d3566623ee242b674b1b7675f
Dmitriy <ulrht88@gmail.com>
2023-05-03 22:29:57 +0300

    patch 3,0

--- script.py
+++ script.py
@@ -4,6 +4,7 @@ import vk_api
 from dotenv import load_dotenv, find_dotenv
 import os
 
+
 load_dotenv(find_dotenv())
 
 TOKEN = os.environ.get('TOKEN')
@@ -14,17 +15,20 @@ vk = session.get_api()
 
 def get_user_friends(user_id):
     friends = session.method('friends.get', {'user_id': user_id})
+
     friend_groups = []
     for friend in friends['items']:
-        user = session.method('users.get', {'user_ids': friend})
-        group = session.method('groups.get', {'user_id': friend})
-        time.sleep(0.4)
-        print(f"{user[0]['first_name']} {user[0]['last_name']}")
-        print(f"{group['items']}")
-        friend_groups.extend(group['items'])
+        try:
+            user = session.method('users.get', {'user_ids': friend})
+            group = session.method('groups.get', {'user_id': friend})
+            time.sleep(0.4)
+            print(f"{user[0]['first_name']} {user[0]['last_name']}")
+            print(f"{group['items']}")
+            friend_groups.extend(group['items'])
+        except vk_api.exceptions.ApiError:
+            friend_groups.append(0)
+
     print('#' * 100)
     print(friend_groups)
-    result = len(friend_groups)
     print('#' * 100)
-    print(f"{'count groups: '}{result}")
     return
